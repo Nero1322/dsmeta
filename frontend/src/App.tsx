f4d6e7c5179989bc3ba7fdada7979cb3d952c18c
nerison morais <nerison.liscano@gmail.com>
2022-11-16 21:35:23 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import notificationbutton from "./assets/componentes/notificationbutton"
 function App() {
-return(
-  <h1>Olá mundo!</h1>
-)  
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <notificationbutton>/
+        <notificationbutton>
+        
+
+      </>
+      )
 }
 
-export default App
+      export default App
---
+++ frontend/src/assets/componentes/notificationbutton/index.tsx
@@ -0,0 +1,11 @@
+import iconnn from '../..assets/img/notification-icon.svg'
+
+import './styles.css'
+function notificationbutton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon}="notification-icon.svg" alt="Notificar" />
+        </div>)
+}
+
+export default notificationbutton
